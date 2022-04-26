c7832344695e3dad3a4430ef79ed91a08e6364f2
Napoleon Lazardi <nrlazardim@gmail.coom>
2022-04-26 23:30:33 +0200

    refactor

--- YahooFinance/main.py
+++ YahooFinance/main.py
@@ -1,15 +1,11 @@
-"""
-Created on Sun Apr 24 16:37:39 2022
-
-@author: NicolasToledo
-"""
-
 import pandas as pd
 import yfinance as yf
 
+
 class YahooAdjPrice:
+
     @staticmethod
-    def getAdjPrice(ticker: str, start_date: str, end_date: str) -> pd.DataFrame():
+    def getAdjPrice(ticker: list, start_date: str, end_date: str) -> pd.DataFrame():
         pxData = pd.DataFrame()
         for t in ticker:
             data = yf.download(t, start=start_date, end_date=end_date)
@@ -18,5 +14,9 @@ class YahooAdjPrice:
             pxData = pxData.merge(data, right_index=True, left_index=True, how="outer")
         return pxData
 
+    @staticmethod
+    def get_dataset_adj_close(instrument: list, start: str, end: str, interval: str):
+        return yf.download(instrument, start, end, interval)['Adj Close']
+
 tickers = ["TSLA","TWTR"]
-pxData = YahooAdjPrice.getAdjPrice(tickers,start_date='2022-01-01',end_date='2022-04-22')
+pxData = YahooAdjPrice.getAdjPrice(tickers, start_date='2022-01-01', end_date='2022-04-22')
