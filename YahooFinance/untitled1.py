c7632f4d3c3f2b48fdfa89dfe08a3e6ea5d52716
Nicolas Toledo <ntoledo@valcereal.com.ar>
2022-04-26 14:48:21 -0300

    Module2

--- YahooFinance/untitled1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 24 16:52:16 2022
-
-@author: NicolasToledo
-"""
-import yfinance as yf
-data = yf.download("AAPL", start="2020-01-01", end_date="2021-01-01")['Adj Close']
